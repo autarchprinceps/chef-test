2764a1c976ac5139e362b474a299b5cd8b8eafb8
autarch princeps <autarch@outlook.com>
2017-03-14 20:16:38 +0100

    FIX

--- cookbooks/awesome_customers_ubuntu/recipes/web_user.rb
+++ cookbooks/awesome_customers_ubuntu/recipes/web_user.rb
@@ -3,10 +3,10 @@
 # Recipe:: web_user
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-group node['awesome_customer_ubuntu']['group']
+group node['awesome_customers_ubuntu']['group']
 
-user node['awesome_customer_ubuntu']['user'] do
-	group node['awesome_customer_ubuntu']['group']
+user node['awesome_customers_ubuntu']['user'] do
+	group node['awesome_customers_ubuntu']['group']
 	system true
 	shell '/bin/bash'
 end
